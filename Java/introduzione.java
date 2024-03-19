38d44784d88c1237f327c2a96c3844c900dd95a4
kolci <kolci@duck.com>
2024-03-19 11:10:56 +0000

    Add hint for lab10 in 'introduzione'

--- Java/introduzione.java
+++ Java/introduzione.java
@@ -9,4 +9,39 @@ public class introduzione{
           public static boolean isFemminile(String s){
                     return (s.charAt(s.length() - 1) == 'a');
           }
+          public class IntSList{
+                    // final == tuple, rimane una costante
+
+                    private final boolean empty;
+
+                    private final int first;
+
+                    private final IntSList rest;
+                    
+                    public IntSList(){
+                              empty = true;
+                              // Non serviva (Lo faceva java)
+                              first = 0;
+                              rest = null;
+                    }
+                    public IntSList(int e, IntSList r){
+                              empty = false;
+                              
+                              first = e;
+                              rest = r;
+                    }
+                    public boolean isNull(){
+                              return empty;
+                    }
+                    public int car(){
+                              return first;
+                    }
+                    public IntSList cdr(){
+                              return rest;
+                    }
+                    public IntSList cons(int e){
+                              // this cambia dinamicamente 
+                              return new IntSList(e, this);
+                    }
+          }
 } 
