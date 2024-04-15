ec0412e25e328ff1ee9a71645028262c4328c343
kolci <kolci@duck.com>
2024-04-15 07:18:55 +0000

    small changes

--- Java/Board.java
+++ Java/Board.java
@@ -2,16 +2,21 @@
  * Queen
  */
 public class Board {
+
+          private final int size;
+          private final int queens;
+          private final BiPredicate<Integer, Integer> attack;
+          private final String config
           public Board(int n){
 
           }
 
           public int size(){
-
+                    return size;
           }
 
           public int queensOn(){
-
+                    return queens;
           }
 
           public boolean underAttack(int i, int j){
@@ -20,5 +25,8 @@ public class Board {
 
           public Board addQueen(int i, int j){
 
+          }
+          public String arrangment(){
+                    return config;
           }
 }
