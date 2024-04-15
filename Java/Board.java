d9295e85d11f4df786859709112be6865336c884
kolci <kolci@duck.com>
2024-04-15 07:57:11 +0000

    completed with some issues

--- Java/Board.java
+++ Java/Board.java
@@ -6,9 +6,18 @@ public class Board {
           private final int size;
           private final int queens;
           private final BiPredicate<Integer, Integer> attack;
-          private final String config
+          private final String config;
           public Board(int n){
+                    size = n;
+                    queens = 0;
+                    attack = (x,y) -> false;
+                    config = " ";
+          }
 
+          private Board(Board b, int i, int j){
+                    size = b.size();
+                    queens = b.queensOn() + 1;
+                    attack = (x, y) -> ((x == i)||(y == j)||(x-y == i-j)||(x+y == i+j)||b.underAttack(x,y));
           }
 
           public int size(){
@@ -20,11 +29,11 @@ public class Board {
           }
 
           public boolean underAttack(int i, int j){
-
+                    return attack.test(i,j);
           }
 
           public Board addQueen(int i, int j){
-
+                    return new Board(this, i, j);
           }
           public String arrangment(){
                     return config;
