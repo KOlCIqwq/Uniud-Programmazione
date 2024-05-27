ea5cb856c605b2a6b395a3eae285ed4710f6b281
kolci <kolci@duck.com>
2024-05-27 20:09:06 +0200

    Made NodeQueue more human readable

--- Java/Huffman/Huff_Lab/NodeQueue.java
+++ Java/Huffman/Huff_Lab/NodeQueue.java
@@ -1,90 +1,83 @@
 package Huffman.Huff_Lab;
 
 class NodeQueue{
-          private static Node[] heap;
-          private static int size;
+    private static Node[] heap;
+    private static int size;
 
-          public NodeQueue(){
-                    heap = new Node[10];
-                    size = 0;
-          }
+    public NodeQueue(){
+      heap = new Node[10];
+      size = 0;
+    }
 
-          public static int size(){
-                    return size;
-          }
+    public static int size(){
+      return size;
+    }
 
-          public static Node peek(){
-                    if (size == 0){
-                              return null;
-                    }
-                    return heap[0];
-          }
+    public static Node peek(){
+      if (size == 0){
+        return null;
+      }
+      return heap[0];
+    }
 
-          public static Node poll(){
-            if (size == 0) {
-              return null;
-            }
-            Node min = heap[0];
-            heap[0] = heap[size - 1];
-            size--;
-            heapifyToggle(0);
-            return min;
-          }
+    public static Node poll(){
+      if (size == 0) {
+        return null;
+      }
+      Node min = heap[0];
+      heap[0] = heap[size - 1];
+      size--;
+      heapifyToggle(0);
+      return min;
+    }
 
-          public static void add(Node n){
-            if (size == heap.length){
-              resize();
-            }
-            heap[size] = n;
-            heapifyadd(size++);
+    public static void add(Node n){
+      if (size == heap.length){
+        resize();
+      }
+      heap[size] = n;
+      heapifyadd(size++);
+    }
+    // Transforming a head into a correct order after adding a new element
+    private static void heapifyadd(int index){
+      // To deepest
+      while (index > 0){
+          // Compare the val of the current leaf with it's parent 
+          int parentIndex = (index - 1) / 2;
+          if (heap[index].Weight() >= heap[parentIndex].Weight()){
+              break;
           }
-          // Transforming a head into a correct order after adding a new element
-          private static void heapifyadd(int index){
-            // To deepest
-            while (index > 0){
-                // Compare the val of the current leaf with it's parent 
-                int parentIndex = (index - 1) / 2;
-                if (heap[index].Weight() >= heap[parentIndex].Weight()){
-                    break;
-                }
-                // If the current is less to it's parent swap them
-                Node temp = heap[index];
-                heap[index] = heap[parentIndex];
-                heap[parentIndex] = temp;
-            }
-          }
+          // If the current is less to it's parent swap them
+          Node temp = heap[index];
+          heap[index] = heap[parentIndex];
+          heap[parentIndex] = temp;
+      }
+    }
 
-          private static void heapifyToggle(int index){
-            while (index < size / 2) {
-              int leftChildIndex = 2 * index + 1;
-              int rightChildIndex = 2 * index + 2;
-              int smallestChildIndex = leftChildIndex;
-  
-              if (rightChildIndex < size && heap[rightChildIndex].Weight() < heap[leftChildIndex].Weight()) {
-                  smallestChildIndex = rightChildIndex;
-              }
-  
-              if (heap[index].Weight() <= heap[smallestChildIndex].Weight()) {
-                  break;
-              }
-              
-              Node temp = heap[index];
-              heap[index] = heap[smallestChildIndex];
-              heap[smallestChildIndex] = temp;
-              index = smallestChildIndex;
-          }
-          }
-
-          private static void swap(int index1, int index2) {
-            Node temp = heap[index1];
-            heap[index1] = heap[index2];
-            heap[index2] = temp;
+    private static void heapifyToggle(int index){
+      while (index < size / 2) {
+        int leftChildIndex = 2 * index + 1;
+        int rightChildIndex = 2 * index + 2;
+        int smallestChildIndex = leftChildIndex;
+    
+        if (rightChildIndex < size && heap[rightChildIndex].Weight() < heap[leftChildIndex].Weight()) {
+            smallestChildIndex = rightChildIndex;
         }
     
+        if (heap[index].Weight() <= heap[smallestChildIndex].Weight()) {
+            break;
+        }
+                
+        Node temp = heap[index];
+        heap[index] = heap[smallestChildIndex];
+        heap[smallestChildIndex] = temp;
+        index = smallestChildIndex;
+      }
+    }
 
-          private static void resize(){
-            Node[] newHeap = new Node[heap.length * 2];
-            System.arraycopy(heap, 0, newHeap, 0, heap.length);
-            heap = newHeap; 
-          }
+    private static void resize(){
+      Node[] newHeap = new Node[heap.length * 2];
+      System.arraycopy(heap, 0, newHeap, 0, heap.length);
+      heap = newHeap; 
+    }
 }
