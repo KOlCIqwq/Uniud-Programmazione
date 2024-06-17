4e7b66e50f3e84322f4f5d9d9ee6a00af5835141
kolci <kolci@duck.com>
2024-06-17 16:08:13 +0200

    1. L22

--- Java/esami/es22_21/L22.java
+++ Java/esami/es22_21/L22.java
@@ -1,27 +1,26 @@
 package esami.es22_21;
 
+import java.util.Arrays;
+
 public class L22 {
     public static void main(String[] args) {
-        
-        System.out.println(heapTest( new double[] {5.0, 3.1, 5.7, 3.1, 8.5, 6.0, 3.0, 4.2, 9.3} ));
+        double[] heap = {5.0, 3.1, 5.7, 3.1, 8.5, 6.0, 3.0, 4.2, 9.3};
+        System.out.println(Arrays.toString(heapTest(heap)));
     }
 
     //1.
-    public static double[] heapTest(double[] list){
-        double[] out = new double[2];
-        for (int i = 0; i < list.length/2; i++){
+    public static int[] heapTest(double[] list){
+        for (int i = 1; i < list.length/2; i++){
             int left = 2 * i;
             int right = left + 1;
             if (list[i] > list[left]){
-                out[0] = list[left];
+                return new int[] {i,left};
             }
-            if (right > list.length){
-                return out;
-            }
-            if (list[i] > list[right]){
-                out[1] = list[right];
+            if (list[i] > list[right] && right < list.length){
+                return new int[] {i, right};
             }
         }
-        return (out == null)? out : null;
+        return null;
     }
 }
+
