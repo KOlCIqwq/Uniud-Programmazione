36e479bd202a22e8a6a586a6bfc3048c0196cd7c
kolci <kolci@duck.com>
2024-06-17 16:43:56 +0200

    Incompleted 2.

--- Java/esami/es22_21/L22.java
+++ Java/esami/es22_21/L22.java
@@ -6,8 +6,9 @@ public class L22 {
     public static void main(String[] args) {
         double[] heap = {5.0, 3.1, 5.7, 3.1, 8.5, 6.0, 3.0, 4.2, 9.3};
         System.out.println(Arrays.toString(heapTest(heap)));
+        double[] s = {5.0, 3.1, 5.7, 3.1, 8.5, 6.0, 3.8, 4.2, 9.3};
+        System.out.println("Length of LLDS is: " + llds(s));
     }
-
     //1.
     public static int[] heapTest(double[] list){
         for (int i = 1; i < list.length/2; i++){
@@ -21,6 +22,29 @@ public class L22 {
             }
         }
         return null;
+    }
+
+    //2.
+    public static int llds(double[] s){
+        int n = s.length;
+        int[][][] mem = new int[n+1][n+1][n+1];
+        for (int i = 0; i <= n; i++){
+            for(int j = 0; j <=n; j++){
+                mem[n][i][j] = 0;
+            }
+        }
+        for (int k = n-1; k >= 1; k--){
+            for(int i = k-1; i >= 1; i--){
+                for (int j = i-1; j >= 1; j--){
+                    if (i == 0 || (Math.min(s[i], s[j]) < s[k] && s[k] < Math.max(s[i], s[j]))){
+                        mem[k][i+1][j+1] = Math.max(1 + mem[k+1][j][k], mem[k+1][i][j]);
+                    } else{
+                        mem[k][i+1][j+1] = mem[k+1][i][j];
+                    }
+                }
+            }
+        }
+        return mem[0][0][0];
     }
 }
 
